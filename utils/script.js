5b564572110c01b6d0e0a69b8308ed15dd24dc10
Tim Friedrich <tim.david007@gmail.com>
2022-02-20 18:30:52 +0100

    navigation bar

---
+++ utils/script.js
@@ -0,0 +1,17 @@
+/**
+ * Bevor JQuery benutzt werden kann, wird die API erst geladen.
+ * Danach wird der eigentlich JQuery-Code ausgeführt (script.onload).
+ */
+var script = document.createElement('script'); 
+document.head.appendChild(script);    
+script.type = 'text/javascript';
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+script.onload = function(){
+    
+    $(document).ready(function (){
+        $.get("../utils/navbar.html", function(data){
+            $("body").prepend(data);
+        });
+    });
+
+} 
